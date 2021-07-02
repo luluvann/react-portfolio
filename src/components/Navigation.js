2e1f85a910288131720f6c0aac7b2da8979687e5
Lucile <developer.lvt@gmail.com>
2021-07-01 21:46:07 -0400

    added logic to highlight selected navbar button

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 function Navigation(props) {
-  const { sections = [], setCurrentSection} = props;
+  const { sections = [], setCurrentSection, currentSection } = props;
 
   const sectionsLis = sections.map((section) => (
     <li key={section.title}>
       <button
+        className={currentSection == section.title && "highlight"}
         onClick={() => {
-          console.log(section.title)
+          console.log(section.title);
           setCurrentSection(section.title);
         }}
       >
