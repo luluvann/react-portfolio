8fab372fda2af18fc801a61b8017f7c73b5169bf
Lucile <developer.lvt@gmail.com>
2021-07-03 19:28:48 -0400

    styled project card

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,20 @@
+import { FaGithub, FaLink } from "react-icons/fa";
+
 function Project(props) {
   return (
     <div className="project-card" key={props.data.title}>
       <h3>
         <a href={props.data.links[1]}>{props.data.title}</a>
       </h3>
+      <p>{props.data.description}</p>
+      <div class="links">
+        <a href={props.data.links[0]}>
+          <FaGithub />
+        </a>
+        <a href={props.data.links[0]}>
+          <FaLink />
+        </a>
+      </div>
     </div>
   );
 }
